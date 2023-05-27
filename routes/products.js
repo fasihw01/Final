17f389653c0769b77af3d1a7a2252bb1dbb7d4db
fasihw01 <119977660+fasihw01@users.noreply.github.com>
2023-05-28 02:57:35 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -7,10 +7,6 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require("aws-sdk");
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  });
   
 
 const s3 = new AWS.S3();
